5cd42c8011443ad5d7b5ab48d80afd2668ac0225
Noah Fahlgren <noahfahlgren@gmail.com>
2015-12-05 15:31:50 -0600

    Create sci-hub.bookmark.js

---
+++ sci-hub.bookmark.js
@@ -0,0 +1,8 @@
+javascript:void(function () {
+	var url = location.href;
+	var parts = url.split('/');
+	parts = parts.slice(2,parts.length - 1);
+	parts[0] = parts[0] + '.sci-hub.io';
+	var newUrl = 'http://' + parts.join('/');
+	location = newUrl;
+}());
