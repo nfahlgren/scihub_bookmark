ab0edb4a41e73a8334ee459ccb885e9a6d89f3cd
ugalaw97 <ugalaw97@gmail.com>
2018-07-06 14:46:04 -0400

    Update sci-hub.bookmark.js
    Changed domain to .tw

--- sci-hub.bookmark.js
+++ sci-hub.bookmark.js
@@ -1,1 +1,1 @@
-javascript:location.href = location.origin.replace(/^https/, 'http') + '.sci-hub.bz' + location.pathname + location.search
+javascript:location.href = location.origin.replace(/^https/, 'http') + '.sci-hub.tw' + location.pathname + location.search
